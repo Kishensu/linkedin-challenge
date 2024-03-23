9ad8fc6b67ea54492811b28fed4d0ff702b28bbd
kishensu <kishensuresh@gmail.com>
2024-03-23 11:31:27 -0400

    Code fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,24 @@
 const profileData = {
-    username: "Kishen S",
-    jobTitle: "Kishen S, Software Engineer",
+    username: "Kishen",
+    jobTitle: "Kishen Suresh Kumar, Software Engineer",
     profilePicture: "./assets/images/083C48D9-3895-43DC-9233-F66BAEB427BF_1_105_c.jpeg"
 };
 
 function updateProfileData() {
 
-const getUsername = document.querySelector('.nav__username');
-const getJobTitle = document.querySelector('.profile-card__title');
-const getProfilePic = document.querySelector('.profile-pic');
+const getUsername = document.querySelectorAll('.newUsername');
+const getJobTitle = document.querySelector('.profileTitle');
+const getProfilePic = document.querySelectorAll('.profile-pic');
 
-getUsername.innerHTML = profileData.username;
+getUsername.forEach(element => {
+    element.innerHTML = profileData.username;
+});
+
 getJobTitle.innerHTML = profileData.jobTitle;
-getProfilePic.src = profileData.profilePicture;
+
+getProfilePic.forEach(element => {
+    element.src = profileData.profilePicture;
+});
 
 }
 
