3ad63d7efc4052a46bea308d24e62f29338bee76
kishensu <kishensuresh@gmail.com>
2024-03-23 10:52:29 -0400

    Updated js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const profileData = {
-    username: "Kishen",
-    jobTitle: "Kishen Suresh Kumar, Software Engineer",
+    username: "Kishen S",
+    jobTitle: "Kishen S, Software Engineer",
     profilePicture: "./assets/images/083C48D9-3895-43DC-9233-F66BAEB427BF_1_105_c.jpeg"
 };
 
@@ -10,8 +10,8 @@ const getUsername = document.querySelector('.nav__username');
 const getJobTitle = document.querySelector('.profile-card__title');
 const getProfilePic = document.querySelector('.profile-pic');
 
-getUsername.textContent = profileData.username;
-getJobTitle.textContent = profileData.jobTitle;
+getUsername.innerHTML = profileData.username;
+getJobTitle.innerHTML = profileData.jobTitle;
 getProfilePic.src = profileData.profilePicture;
 
 }
